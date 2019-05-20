69c42595fde1e1d1cc46a89c80dcc5e9133778f5
pramesywaraj <pramesywara_jp@apps.ipb.ac.id>
2019-05-20 23:26:04 +0700

    adding basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppsidemenuComponent } from './components/appsidemenu/appsidemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppsidemenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/appsidemenu/appsidemenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appsidemenu',
+  templateUrl: './appsidemenu.component.html',
+  styleUrls: ['./appsidemenu.component.scss']
+})
+export class AppsidemenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
