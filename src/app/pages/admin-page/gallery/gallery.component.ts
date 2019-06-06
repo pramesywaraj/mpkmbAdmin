4822233655ca16557211d181a054193b0975fe60
pramesywaraj <pramesywara_jp@apps.ipb.ac.id>
2019-06-07 04:35:12 +0700

    add image almost completed

--- src/app/pages/admin-page/gallery/gallery.component.ts
+++ src/app/pages/admin-page/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GalleryService } from '../../../services/gallery.service';
 
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
@@ -14,12 +15,20 @@ export class GalleryComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
 
   videosList = [];
+  photosList = [];
+  categoriesList = [];  
   
-  constructor(private gallery: GalleryService) { }
+  constructor(private gallery: GalleryService, private router: Router) { }
 
   ngOnInit() {
     this.initTheTab();
     this.getVideos();
+    this.getPhotos();
+    this.getCategories();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   initTheTab() {
@@ -27,17 +36,106 @@ export class GalleryComponent implements OnInit, OnDestroy {
     console.log('awa', this.viewTab);
   }
 
-  getVideos() {
-    this.subscription = this.gallery.getVideosList().subscribe(data => {
+  async getVideos() {
+    this.subscription = await this.gallery.getVideosList().subscribe(data => {
       if(data.status == 200) {
         this.videosList = data.videoGaleries;
-        console.log('video', this.videosList);
       }
     });
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  async getPhotos() {
+    this.subscription = await this.gallery.getPhotosList().subscribe(data => {
+      if(data.status == 200) {
+        this.photosList = data.imageGalery;
+      }
+    });
+  }
+
+  async getCategories() {
+    this.subscription = await this.gallery.getCategories().subscribe(data => {
+      if(data.status == 200) {
+        this.categoriesList = data.categories;
+      }
+    });
+  }
+
+  addPhotoCategories(categories) {
+    if(categories) {
+      let temp = categories.value;
+      let objArray = {
+        'name' : temp.categoryName
+      };
+      
+      console.log('wwww', objArray);
+      this.gallery.addCategory(objArray).subscribe(
+        data => {
+          if(data.status == 201) {
+            alert(data.message);
+            this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+              this.router.navigate(["admin/galeri"])
+            ); 
+          }
+        },
+        err => {
+          console.log('err', err);
+        }
+      );
+    } else {
+      alert('Anda belum memasukkan nama kategori!');
+    }
+  }
+
+  addVideo(video) {
+    if(video) {
+      let temp = video.value;
+      let objArray = {
+        'title' : temp.videoTitle,
+        'url' : temp.videoUrl
+      };
+      
+      this.gallery.addVideo(objArray).subscribe(
+        data => {
+          if(data.status == 201) {
+            alert(data.message);
+            this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+              this.router.navigate(["admin/galeri"])
+            ); 
+          }
+        },
+        err => {
+          console.log('err', err);
+        }
+      );
+    } else {
+      alert('Anda belum mengisi form tambah video!');
+    }
+  }
+
+  addPhoto(photo) {
+    if(photo) {
+      let temp = photo.value;
+      let objArray = {
+        'categoryId' : temp.categoryId,
+        'url' : temp.photoUrl
+      };
+      
+      this.gallery.addPhoto(objArray).subscribe(
+        data => {
+          if(data.status == 201) {
+            alert(data.message);
+            this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+              this.router.navigate(["admin/galeri"])
+            ); 
+          }
+        },
+        err => {
+          console.log('err', err);
+        }
+      );
+    } else {
+      alert('Anda belum mengisi form tambah video!');
+    }
   }
 
 }
--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -32,4 +32,98 @@ export class GalleryService {
       })
     ); 
   }
+
+  public getPhotosList(): Observable<any> {
+    return this.http.get<any>(
+      this.config.baseUrl + 'galery/image/list'
+    ).pipe(
+      map(
+        resp => {
+          return resp;
+        }
+      ),
+      catchError(err => {
+        console.log('This error inside the gallery service and getPhotosList function...', err);
+        return throwError(err);
+      })
+    ); 
+  }
+  
+  public getCategories(): Observable<any> {
+    return this.http.get<any>(
+      this.config.baseUrl + 'galery/category/get'
+    ).pipe(
+      map(
+        resp => {
+          return resp;
+        }
+      ),
+      catchError(err => {
+        console.log('This error inside the gallery service and getCategories function...', err);
+        return throwError(err);
+      })
+    ); 
+  }
+
+
+  public addVideo(video): Observable<any> {
+    let header = new HttpHeaders();
+    header = header.append('Content-Type', 'application/json');
+    header = header.append('Authorization', 'Bearer ' + this.userToken);
+
+    return this.http.post<any>(
+      this.config.baseUrl + 'galery/video/create', JSON.stringify(video), {headers: header})
+      .pipe(
+        map(
+          resp => {
+            return resp;
+          }
+        ),
+        catchError(err => {
+          console.log('This error inside the gallery service and addVideo function...', err);
+          return throwError(err);
+        })
+      );
+  }
+
+  public addPhoto(photo): Observable<any> {
+    let header = new HttpHeaders();
+    header = header.append('Content-Type', 'application/json');
+    header = header.append('Authorization', 'Bearer ' + this.userToken);
+
+    return this.http.post<any>(
+      this.config.baseUrl + 'galery/image/create', JSON.stringify(photo), {headers: header})
+      .pipe(
+        map(
+          resp => {
+            return resp;
+          }
+        ),
+        catchError(err => {
+          console.log('This error inside the gallery service and addImage function...', err);
+          return throwError(err);
+        })
+      );
+  }
+
+  public addCategory(category): Observable<any> {
+    let header = new HttpHeaders();
+    header = header.append('Content-Type', 'application/json');
+    header = header.append('Authorization', 'Bearer ' + this.userToken);
+
+    return this.http.post<any>(
+      this.config.baseUrl + 'galery/category/create', JSON.stringify(category), {headers: header})
+      .pipe(
+        map(
+          resp => {
+            console.log('debug', resp);
+            return resp;
+          }
+        ),
+        catchError(err => {
+          console.log('This error inside the gallery service and addCategory function...', err);
+          return err;
+        })
+      );
+  }
 }
